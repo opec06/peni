7cd032ac6019eacfd805e4ced22725b5a0069e6e
Opeyemi Adebola <opec06@gmail.com>
2022-08-18 17:38:22 +0100

    update to all

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+var faq = document.getElementsByClassName("faq-page");
+var i;
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
 }
